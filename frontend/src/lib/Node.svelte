b54593a300465d0c3159f9b2fcadfccf0bb0c9d8
twsnmp <ymi@twise.co.jp>
2023-09-28 06:54:43 +0900

    ノード編集ページの多言語対応

--- frontend/src/lib/Node.svelte
+++ frontend/src/lib/Node.svelte
@@ -13,6 +13,7 @@
   import * as icons from "@mdi/js";
   import type { datastore } from "wailsjs/go/models";
   import { addrModeList, getIcon, iconList, snmpModeList } from "./common";
+  import { _ } from "svelte-i18n";
 
   export let nodeID: string = "";
   export let posX = 0;
@@ -28,7 +29,7 @@
     } else {
       node = {
         ID: "",
-        Name: "新規ノード",
+        Name: $_('Node.NewNode'),
         Descr: "",
         Icon: "",
         State: "",
@@ -68,58 +69,56 @@
 
 <Modal bind:open={show} size="lg" permanent class="w-full" on:on:close={close}>
   <form class="flex flex-col space-y-4" action="#">
-    <h3 class="mb-1 font-medium text-gray-900 dark:text-white">ノードの編集</h3>
+    <h3 class="mb-1 font-medium text-gray-900 dark:text-white">{ $_('Node.EditNode') }</h3>
     <div class="grid gap-4 mb-4 md:grid-cols-3">
       <Label class="space-y-2">
-        <span>名前</span>
+        <span>{ $_('Node.Name') }</span>
         <Input
           bind:value={node.Name}
-          placeholder="ノード名"
           required
           size="sm"
         />
       </Label>
       <Label class="space-y-2">
-        <span>IPアドレス</span>
+        <span>{ $_('Node.IPAddress') }</span>
         <Input
           bind:value={node.IP}
-          placeholder="IPアドレス"
           required
           size="sm"
         />
       </Label>
       <Label class="space-y-2">
-        <span> アドレスモード </span>
+        <span> { $_('Node.AddressMode') } </span>
         <Select
           items={addrModeList}
           bind:value={node.AddrMode}
-          placeholder="アドレスモードを選択"
+          placeholder={ $_('Node.SelectAddressMode') }
           size="sm"
         />
       </Label>
     </div>
     <div class="grid gap-4 mb-4 md:grid-cols-3">
       <Label class="space-y-2">
-        <span> アイコン </span>
+        <span> { $_('Node.Icon') } </span>
         <Select
           items={iconList}
           bind:value={node.Icon}
-          placeholder="アイコンを選択"
+          placeholder={ $_('Node.SelectIcon') }
           size="sm"
         />
       </Label>
       <div class="mt-5 ml-5">
         <span class="mdi {getIcon(node.Icon)} text-4xl" />
       </div>
-      <Checkbox bind:checked={node.AutoAck}>復帰時に自動確認</Checkbox>
+      <Checkbox bind:checked={node.AutoAck}>{ $_('Node.AutoCheck') }</Checkbox>
     </div>
     <div class="grid gap-4 md:grid-cols-2">
       <Label class="space-y-2">
-        <span> SNMPモード </span>
+        <span> { $_('Node.SNMPMode') } </span>
         <Select
           items={snmpModeList}
           bind:value={node.SnmpMode}
-          placeholder="SNMPのモードを選択"
+          placeholder="{ $_('Node.SelectSnmpMode') }"
           size="sm"
         />
       </Label>
@@ -130,39 +129,38 @@
     </div>
     <div class="grid gap-4 md:grid-cols-2">
       <Label class="space-y-2">
-        <span>ユーザー</span>
-        <Input bind:value={node.User} placeholder="ユーザー名" size="sm" />
+        <span>{ $_('Node.SnmpUser') }</span>
+        <Input bind:value={node.User} size="sm" />
       </Label>
       <Label class="space-y-2">
-        <span>SNMPパスワード</span>
+        <span>{ $_('Node.SnmpPassword') }</span>
         <Input
           type="password"
           bind:value={node.Password}
-          placeholder="•••••"
           size="sm"
         />
       </Label>
     </div>
     <Label class="space-y-2">
-      <span>公開鍵</span>
-      <Input bind:value={node.PublicKey} placeholder="公開鍵" size="sm" />
+      <span>{ $_('Node.PublicKey') }</span>
+      <Input bind:value={node.PublicKey} size="sm" />
     </Label>
     <Label class="space-y-2">
       <span>URL</span>
       <Input bind:value={node.URL} placeholder="URL" size="sm" />
     </Label>
     <Label class="space-y-2">
-      <span>説明</span>
-      <Input bind:value={node.Descr} placeholder="説明" size="sm" />
+      <span>{ $_('Node.Descr') }</span>
+      <Input bind:value={node.Descr} size="sm" />
     </Label>
     <div class="flex justify-end space-x-2 mr-2">
       <Button color="blue" type="button" on:click={save} size="sm">
         <Icon path={icons.mdiContentSave} size={1} />
-        保存
+        { $_('Node.Save') }
       </Button>
       <Button type="button" color="alternative" on:click={close} size="sm">
         <Icon path={icons.mdiCancel} size={1} />
-        キャンセル
+        { $_('Node.Cancel') }
       </Button>
     </div>
   </form>
