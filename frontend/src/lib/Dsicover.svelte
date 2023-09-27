5f6a1f62af98fefa0f229c802a2e8926ba2301a7
twsnmp <ymi@twise.co.jp>
2023-09-28 05:24:37 +0900

    自動発見のページの多言語対応と表示改善

--- frontend/src/lib/Dsicover.svelte
+++ frontend/src/lib/Dsicover.svelte
@@ -16,6 +16,8 @@
   } from "../../wailsjs/go/main/App";
   import Icon from "mdi-svelte";
   import * as icons from "@mdi/js";
+  import { _ } from "svelte-i18n";
+
   export let posX = 0;
   export let posY = 0;
 
@@ -23,27 +25,34 @@
   let conf = undefined;
   let showConf = false;
   let showStats = false;
+  let showStop = true;
   let timer: number | undefined = undefined;
   const dispatch = createEventDispatcher();
 
   const updateDiscover = async () => {
     stats = await GetDiscoverStats();
-    showStats = stats.Running;
-    showConf = !stats.Running;
     if (!stats.Running) {
       timer = undefined;
-      return;
+      showStop = false;
+      return false;
     }
     timer = setTimeout(() => {
       updateDiscover();
     }, 2 * 1000);
+    return true;
   };
 
   onMount(async () => {
     conf = await GetDiscoverConf();
     conf.X = posX;
     conf.Y = posY;
-    updateDiscover();
+    if (await updateDiscover()) {
+      showStats = true;
+      showConf = false;
+    } else {
+      showStats = false;
+      showConf = true;
+    }
   });
 
   onDestroy(() => {
@@ -60,42 +69,37 @@
   const start = async () => {
     const r = await StartDiscover(conf);
     if (r) {
-      close();
+      showStats = true;
+      showConf = false;
+      showStop = true;
+      updateDiscover();
     }
   };
 
   const stop = async () => {
+    showStop = false;
     await StopDiscover();
-    close();
   };
 </script>
 
 <Modal bind:open={showConf} size="lg" permanent class="w-full">
   <form class="flex flex-col space-y-4" action="#">
-    <h3 class="mb-1 font-medium text-gray-900 dark:text-white">自動発見</h3>
+    <h3 class="mb-1 font-medium text-gray-900 dark:text-white">
+      {$_("Discover.Discover")}
+    </h3>
     <div class="grid gap-4 mb-4 md:grid-cols-2">
       <Label class="space-y-2">
-        <span>開始IP</span>
-        <Input
-          bind:value={conf.StartIP}
-          placeholder="開始IP"
-          required
-          size="sm"
-        />
+        <span>{$_("Discover.StartIP")}</span>
+        <Input bind:value={conf.StartIP} required size="sm" />
       </Label>
       <Label class="space-y-2">
-        <span>終了IP</span>
-        <Input
-          bind:value={conf.EndIP}
-          placeholder="終了IP"
-          required
-          size="sm"
-        />
+        <span>{$_("Discover.EndIP")}</span>
+        <Input bind:value={conf.EndIP} required size="sm" />
       </Label>
     </div>
     <div class="grid gap-4 mb-4 md:grid-cols-2">
       <Label class="space-y-2">
-        <span> タイムアウト(秒) </span>
+        <span> {$_("Discover.Timeout")} </span>
         <Input
           type="number"
           min={1}
@@ -106,7 +110,7 @@
         />
       </Label>
       <Label class="space-y-2">
-        <span> リトライ(回) </span>
+        <span> {$_("Discover.Retry")} </span>
         <Input
           type="number"
           min={0}
@@ -118,24 +122,27 @@
       </Label>
     </div>
     <div class="grid gap-4 mb-4 md:grid-cols-2">
-      <Checkbox bind:checked={conf.PortScan}>ポートスキャン</Checkbox>
-      <Checkbox bind:checked={conf.AddPolling}>ポーリング自動設定</Checkbox>
+      <Checkbox bind:checked={conf.PortScan}>{$_("Discover.PortScan")}</Checkbox
+      >
+      <Checkbox bind:checked={conf.AddPolling}
+        >{$_("Discover.AutoAddPolling")}</Checkbox
+      >
     </div>
     <div class="flex justify-end space-x-2 mr-2">
       <Button type="button" on:click={start} size="sm">
-        <Icon path={icons.mdiRun} size={1} />
-        開始
+        <Icon path={icons.mdiSearchWeb} size={1} />
+        {$_("Discover.Start")}
       </Button>
       <Button type="button" color="alternative" on:click={close} size="sm">
         <Icon path={icons.mdiCancel} size={1} />
-        閉じる
+        {$_("Discover.Close")}
       </Button>
     </div>
   </form>
 </Modal>
 <Modal bind:open={showStats} size="lg" permanent class="w-full">
   <h3 class="mb-1 font-medium text-gray-900 dark:text-white">
-    自動発見の進行状況
+    {$_("Discover.Stats")}
   </h3>
   <div class="flex flex-col space-y-4">
     <Progressbar
@@ -163,39 +170,83 @@
       labelOutside="SNMP:{stats.Snmp + '/' + stats.Found}"
     />
     {#if conf.PortScan}
-      <Progressbar
-        progress={(stats.Found
-          ? ((100 * stats.Web) / stats.Found).toFixed(2)
-          : 0) + ""}
-        color="gray"
-        size="h-5"
-        labelOutside="Web:{stats.Web + '/' + stats.Found}"
-      />
-      <Progressbar
-        progress={(stats.Found
-          ? ((100 * stats.Mail) / stats.Found).toFixed(2)
-          : 0) + ""}
-        color="gray"
-        size="h-5"
-        labelOutside="Mail:{stats.Mail + '/' + stats.Found}"
-      />
-      <Progressbar
-        progress={(stats.Found
-          ? ((100 * stats.SSH) / stats.Found).toFixed(2)
-          : 0) + ""}
-        color="gray"
-        size="h-5"
-        labelOutside="SSH:{stats.SSH + '/' + stats.Found}"
-      />
+      <div class="grid gap-2 grid-cols-2">
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.Web) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="Web:{stats.Web + '/' + stats.Found}"
+          />
+        </div>
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.Mail) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="Mail:{stats.Mail + '/' + stats.Found}"
+          />
+        </div>
+      </div>
+      <div class="grid gap-2 grid-cols-2">
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.SSH) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="SSH:{stats.SSH + '/' + stats.Found}"
+          />
+        </div>
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.File) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="File:{stats.File + '/' + stats.Found}"
+          />
+        </div>
+      </div>
+      <div class="grid gap-2 grid-cols-2">
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.RDP) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="RDP/VNC:{stats.RDP + '/' + stats.Found}"
+          />
+        </div>
+        <div>
+          <Progressbar
+            progress={(stats.Found
+              ? ((100 * stats.LDAP) / stats.Found).toFixed(2)
+              : 0) + ""}
+            color="gray"
+            size="h-5"
+            labelOutside="LDAP/AD:{stats.SSH + '/' + stats.Found}"
+          />
+        </div>
+      </div>
     {/if}
     <div class="flex justify-end space-x-2 mr-2">
-      <Button type="button" color="red" on:click={stop} size="sm">
-        <Icon path={icons.mdiRun} size={1} />
-        停止
-      </Button>
+      {#if showStop}
+        <Button type="button" color="red" on:click={stop} size="sm">
+          <Icon path={icons.mdiStop} size={1} />
+          {$_("Discover.Stop")}
+        </Button>
+      {/if}
       <Button type="button" color="alternative" on:click={close} size="sm">
         <Icon path={icons.mdiCancel} size={1} />
-        閉じる
+        {$_("Discover.Close")}
       </Button>
     </div>
   </div>
