3726db3f1f8556b8f2ad9a9d88b1c144957b8110
twsnmp <ymi@twise.co.jp>
2023-09-28 16:24:38 +0900

    TRAPレポートページの多言語対応

--- frontend/src/lib/TrapReport.svelte
+++ frontend/src/lib/TrapReport.svelte
@@ -6,6 +6,7 @@
   import type { datastore } from "wailsjs/go/models";
   import {showTrapFromAddr,showTrapLog3D,showTrapTypeChart } from "./chart/trap";
   import { showLogHeatmap } from "./chart/eventlog";
+  import { _ } from "svelte-i18n";
 
   export let logs : datastore.TrapEnt[] | undefined =undefined;
   let show: boolean = false;
@@ -52,28 +53,28 @@
       <TabItem open on:click={()=>{showChart("type")}}>
         <div slot="title" class="flex items-center gap-2">
           <Icon path={icons.mdiChartPie} size={1} />
-          TRAP種類別
+          {$_('TrapReport.CountByType')}
         </div>
         <div id="type" style="height: 500px;"></div>
       </TabItem>
       <TabItem on:click={()=>{showChart("heatmap")}}>
         <div slot="title" class="flex items-center gap-2">
           <Icon path={icons.mdiChartBox} size={1} />
-          ヒートマップ
+          {$_('TrapReport.Heatmap')}
         </div>
         <div id="heatmap" style="height: 500px;"></div>
       </TabItem>
       <TabItem on:click={()=>{showChart("from")}}>
         <div slot="title" class="flex items-center gap-2">
           <Icon path={icons.mdiChartBarStacked} size={1} />
-          送信元アドレス別
+          {$_('TrapReport.CountByFromAddress')}
         </div>
         <div id="from" style="height: 500px;"></div>
       </TabItem>
       <TabItem on:click={()=>{showChart("trap3D")}}>
         <div slot="title" class="flex items-center gap-2">
           <Icon path={icons.mdiChartScatterPlot} size={1} />
-          送信元と種別(3D)
+          {$_('TrapReport.Chart3D')}
         </div>
         <div id="trap3D" style="height: 500px;"></div>
       </TabItem>
@@ -81,7 +82,7 @@
     <div class="flex justify-end space-x-2 mr-2">
       <Button type="button" color="alternative" on:click={close} size="sm">
         <Icon path={icons.mdiCancel} size={1} />
-        閉じる
+        {$_('TrapReport.Close')}
       </Button>
     </div>
   </div>
