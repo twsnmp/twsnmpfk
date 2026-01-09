2b9a74e925118b9af43463dcac4f2abf33da764e
twsnmp <ymi@twise.co.jp>
2026-01-09 09:10:48 +0900

    SNMPv1 TRAPの表示を改善

--- datastore/log.go
+++ datastore/log.go
@@ -781,7 +781,26 @@ func ForEachTraps(st, et int64, f func(*TrapEnt) bool) error {
 				if spe, ok = sl["SpecificTrap"].(float64); !ok {
 					continue
 				}
-				re.TrapType = fmt.Sprintf("%s:%d:%d", ent, int(gen), int(spe))
+				switch int(gen) {
+				case 0:
+					re.TrapType = "coldStart(v1)"
+				case 1:
+					re.TrapType = "warmStart(v1)"
+				case 2:
+					re.TrapType = "linkDown(v1)"
+				case 3:
+					re.TrapType = "linkUp(v1)"
+				case 4:
+					re.TrapType = "authenticationFailure(v1)"
+				case 5:
+					re.TrapType = "egpNeighborLoss(v1)"
+				default:
+					re.TrapType = fmt.Sprintf("enterpriseSpecific(%d)", int(spe))
+				}
+				if re.Variables != "" {
+					re.Variables += ","
+				}
+				re.Variables += "Enterprise=" + ent
 			}
 			re.Time = l.Time
 			if !f(re) {
